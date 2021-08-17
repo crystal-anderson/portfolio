84e541cfcb591bde1bbacad2f4be97634a0b961c
Crystal Anderson <cryssann@gmail.com>
2021-08-17 14:30:29 -0600

    styled components installed, router installed, bheader created, temp background added

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
